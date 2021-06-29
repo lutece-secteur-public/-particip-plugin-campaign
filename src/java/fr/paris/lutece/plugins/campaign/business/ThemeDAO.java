c35f73adfbad9ffd61cac279fe35c13bbc34c1d4
Felix Dupriez <felix.dupriez@accenture.com>
2021-06-29 15:19:09 +0200

    [PCAMPAIGN-2] Correctif requete SQL_QUERY_SELECTALL_BY_CAMPAGNE

--- src/java/fr/paris/lutece/plugins/campaign/business/ThemeDAO.java
+++ src/java/fr/paris/lutece/plugins/campaign/business/ThemeDAO.java
@@ -57,7 +57,7 @@ public final class ThemeDAO implements IThemeDAO
     private static final String SQL_QUERY_DELETE = "DELETE FROM campaign_theme WHERE id_theme = ? ";
     private static final String SQL_QUERY_UPDATE = "UPDATE campaign_theme SET id_theme = ?, campaign_code = ?, code_theme = ?, title = ?, description = ?, active = ?, front_rgb = ?, image_file = ? WHERE id_theme = ?";
     private static final String SQL_QUERY_SELECTALL = "SELECT id_theme, campaign_code, code_theme, title, description, active, front_rgb, image_file FROM campaign_theme";
-    private static final String SQL_QUERY_SELECTALL_BY_CAMPAGNE = SQL_QUERY_SELECTALL + " WHERE code_campagne = ?";
+    private static final String SQL_QUERY_SELECTALL_BY_CAMPAGNE = SQL_QUERY_SELECTALL + " WHERE campaign_code = ?";
     private static final String SQL_QUERY_SELECTALL_ID = "SELECT id_theme FROM campaign_theme";
     private static final String SQL_QUERY_SELECT_BY_TITLETHEME = "SELECT id_theme, campaign_code, code_theme, title, description, active, front_rgb, image_file FROM campaign_theme WHERE code_theme = ?";
 
